463eafb97a01899a7c246c7481b1a3952d212b22
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-03-23 08:24:50 +0530

    Redux store is created

---
+++ src/state/store.js
@@ -0,0 +1,4 @@
+import { applyMiddleware, createStore } from "redux";
+import reducers from "./Reducers"
+import { thunk } from "redux-thunk";
+export const store = createStore(reducers,{},applyMiddleware(thunk))
